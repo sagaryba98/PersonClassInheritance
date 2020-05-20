e25008f211d8e6a2c90ccf2d1b08117df40e6a5e
sagarwaiba <sagarwaiba@sagars-mbp>
2020-05-19 19:34:30 -0500

    Updated the person , student and staff class

---
+++ PersonClassInheritance/src/Person.java
@@ -0,0 +1,33 @@
+
+public class Person {
+
+	private String name;
+	private String address;
+	
+	public Person() {
+		
+	}
+	
+	public Person(String name, String address) {
+		this.name=name;
+		this.address=address;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getAdress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address=address;
+		
+	}
+	
+	public String toString() {
+		return "Person[name="+name+",address="+address+"]";
+	}
+	
+	
+}
---
+++ PersonClassInheritance/src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		Person p = new Person("Sagar", "Irving");
+		System.out.println(p);
+		
+		Student s = new Student("Suman", "Garnd Parie", "Bachlor", 2020, 2020.20);
+		System.out.println(s);
+		
+		Staff sf=new  Staff("Suraj", "Arlington", "Texa A&M", 2021);
+		System.out.println(sf);
+		
+		Person st = new Student("suman", "irving", "B.Ed", 2018, 1010.22);
+		System.out.println(st);
+		
+
+	}
+
+}
